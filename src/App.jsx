dafdfb45dd0e853ed0db2312c856d6b6b3533531
Urmila Rajbanshi <urmilarajbanshi123@gmail.com>
2023-06-12 12:30:46 +0545

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import React,{ useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import ResponsiveAppBar from './User/UserLayout';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import UserLayout from './User/UserLayout';
+import Home from './User/Home/Home';
+import About from './User/AboutUs/About';
+import Blog from './User/Blog/Blog';
+
 
 
 function App() {
  return (<>
   <Router>
     <Routes>
-      <Route path="" element={<ResponsiveAppBar/>}></Route>
+      <Route path="" element={<UserLayout/>}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/blog" element={<Blog/>}/>
+      </Route>
     </Routes>
   </Router>
   </>
