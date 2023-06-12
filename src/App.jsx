a0bcadf1d801ad857f45c202b8dc77c664744cff
Urmila Rajbanshi <urmilarajbanshi123@gmail.com>
2023-06-12 09:09:24 +0545

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React,{ useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ResponsiveAppBar from './User/UserLayout';
 
 
 function App() {
  return (<>
-  <ResponsiveDrawer/>
+  <ResponsiveAppBar/>
   </>
  );
 }
